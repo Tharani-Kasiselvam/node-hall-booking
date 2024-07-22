ef6c1533175588940d428134ec51bb6c8460ed82
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-07-22 19:36:02 +0530

    added logger-morgan module

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
 
 //middleware to parse the request body
 app.use(express.json())
+
+//middleware to log the request
+app.use(morgan('dev'))
 
 app.get('/',(req,res)=>{
    res.send("Hall Booking System")
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const app = require('./app')
 
-app.listen(3050, () => {
-    console.log("Server running on http://localhost:3050/")
+const mongoose = require('mongoose')
+
+console.log("Connecting to MongoDB")
+
+mongoose.connect('mongodb+srv://sherleytharani:Guvi2024@mangodbtmj.rmeyv9c.mongodb.net')
+.then(
+    () => {
+        console.log("CONNECTED to MongoDB")
+        app.listen(3050, () => {
+            console.log("Server running on http://localhost:3050")
+        })
+    }
+).catch(error => {
+    console.error('Error connecting to MongoDB')
 })
+
